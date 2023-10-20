c1786253156fbadf0eebee42c0acaf3fd1d53549
Tintu Tom <tintutom2000@gmail.com>
2023-10-20 14:26:23 +0530

    Inhome Doctor Consulting Userside Authentication with an error in authenticate with Google

---
+++ backend/inhome_consulting/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for inhome_consulting project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'inhome_consulting.settings')
+
+application = get_asgi_application()
---
+++ frontend/src/App.js
@@ -0,0 +1,30 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './containers/Home'
+import Login from './containers/Login'
+import ResetPassword from './containers/ResetPassword'
+import ResetPasswordConfirm from './containers/ResetPasswordConfirm'
+import Signup from './containers/Signup'
+import Activate from './containers/Activate'
+import { Provider } from 'react-redux'
+import Layout from './hocs/Layout' // You should uncomment this line to use the Layout component
+import store from './store'
+import Google from './containers/Google'
+
+const App = () => (
+    <Provider store={store}>
+      <Router>
+          <Layout> 
+            <Routes>
+                <Route exact path='/' element={<Home />} />
+                <Route path='/login' element={<Login />} />
+                <Route path='/signup' element={<Signup />} />
+                <Route path='/google' component={<Google />} />
+                <Route path='/reset-password' element={<ResetPassword />} />
+                <Route path='/password/reset/confirm/:uid/:token' element={<ResetPasswordConfirm />} />
+                <Route path='/activate/:uid/:token' element={<Activate/>} />
+            </Routes>
+          </Layout>
+      </Router>
+    </Provider>
+);
+export default App;
